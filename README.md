7aab74dd060e9db54b0c8b28cbfe36dbac981af7
Scripthqs <1941186772@qq.com>
2021-07-29 09:34:59 +0800

    封装mockjs
    Signed-off-by: Scripthqs <1941186772@qq.com>

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
 # mock
 
-## Project setup
+## 一、在项目中安装mockjs和axios
 ```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
+npm install mockjs --save
+npm install axios --save
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+### 二、文件结构：
 
-### Lints and fixes files
-```
-npm run lint
-```
+1.在项目src根目录创建mock文件夹，与main.js同级。mock文件夹下创建index.js主文件，设置拦截请求，配置请求路径，请求方式等。
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+2.mock文件夹下除了index配置文件，剩下的就是具体到业务的数据规则，可以根据自己的习惯创建。例如首页的数据放在homeMock.js中，登录所需数据放在loginMock.js中。
+
+3.在main.js引入mock配置文件。
+
+4.在根目录下创建一个api文件夹，其中index文件用于封装axios请求,其他文件处理具体业务的请求逻辑。
+
+5.在组件中获取数据
+
+### 三、官方mockjs文档
+[Mock.js官方文档](http://mockjs.com/).
+
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
   <div id="app">
-    {{data}}
+    {{ data }}
   </div>
 </template>
 
 <script>
-import  {getHomeMock} from './api/home'
+import { getHomeMock } from "./api/home";
 
 export default {
-      data() {
-        return {
-          data:[]
-        }
-      },
-      created() {
-        this.getHomeMock()
-      },
-      methods:{
-        getHomeMock(){
-         getHomeMock().then(res => {
-            console.log(res);
-            this.data = res.data.title
-          })
-        }
-        }
-}
+  data() {
+    return {
+      data: [],
+    };
+  },
+  created() {
+    this.getHomeMock();
+  },
+  methods: {
+    getHomeMock() {
+      getHomeMock().then((res) => {
+        console.log(res);
+        this.data = res;
+      });
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
